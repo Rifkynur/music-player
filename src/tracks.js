6e708630e1aa143171712cc4c27aa94535ca1ce0
rifkyNur <machvory@gmail.com>
2025-11-22 15:08:25 +0700

    first commit

---
+++ src/tracks.js
@@ -0,0 +1,12 @@
+export const tracks = [
+  {
+    id: 1,
+    title: "Neon Lights",
+    artist: "Cyber Pop",
+    duration: 189,
+    coverUrl:
+      "https://images.pexels.com/photos/1370704/pexels-photo-1370704.jpeg",
+    audioUrl:
+      "https://www.soundjay.com/free-music/sounds/midnight-ride-01a.mp3",
+  },
+];
